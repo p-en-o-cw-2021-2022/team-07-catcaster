fef9be9e902fc82cf33b321ab50a308bcece8da2
Jef Vrancken <jef.vrancken@student.kuleuven.be>
2022-02-18 16:11:40 +0100

    QR code generaten met gegeven URL

---
+++ src/qr/qr.ts
@@ -0,0 +1,14 @@
+const qr = require('qrcode');
+
+export function generateQR(url: String) {
+
+    qr.toFile("QR.png", url, {type:'png'}, function (err:any, data:any) {
+        if (err) {
+        console.error('Error:', err.message)
+        process.exit(1)
+        }
+
+        console.log('saved qrcode to: ' + "QR.png" + '\n')
+    })
+}
+  
