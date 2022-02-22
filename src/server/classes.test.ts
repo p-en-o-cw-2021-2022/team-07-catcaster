e7c480e76d925b40c93519fbebcad16bec0de49f
Warre Heylen <warre.heylen@gmail.com>
2022-02-22 17:36:33 +0100

    improved classes

--- src/server/classes.test.ts
+++ src/server/classes.test.ts
@@ -2,4 +2,11 @@ import {StringRandomizer} from './classes';
 
 describe('IDgenerator', () => {
     
+    const idGenerator = new StringRandomizer();
+
+    test('Testing the generation of a string of 8 hexadecimal characters', () => {
+        const id = idGenerator.generateId(8);
+        expect(id.length).toEqual(8);        
+    
+    })
 })
--- src/server/classes.ts
+++ src/server/classes.ts
@@ -1,17 +1,39 @@
 /*
 Classes used by the server.
+Written by Warre.
 */
-class StringRandomizer
-{
-   public stringChars:string = "0123456789abcdef";
 
-   public GenerateUniqueHexString(length:number)
-   {
-      let result:string = "";
-      for(let i = 0; i<length;i++) {
-        let character:string = this.stringChars[i];
-        result = result + character;
-      }
-      return result;
-   }
+/*
+This class stores all ID's and helps to check if they are unique.
+*/
+export class IdDatabase
+{
+    public ids:any = new Map();
+
+    public stringChars:string = "0123456789abcdef";
+
+    public generateId(length:number)
+    {
+        let result:string = "";
+        while(!this.doesIdExist(result)){
+            for(let i = 0; i<length;i++) {
+                let character:string = this.stringChars[i];
+                result = result + character;
+            }
+        }
+        return result;
+    }
+
+    public doesIdExist(id:string) {
+        if(this.ids.has(id)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public addScreen(id:string) {
+        
+    }
 }
