cfeb0b84f533402e7fc2a13c8fa75fcc1d2095c0
Warre Heylen <warre.heylen@gmail.com>
2022-02-22 17:44:23 +0100

    added documentation

--- src/server/classes.test.ts
+++ src/server/classes.test.ts
@@ -2,11 +2,4 @@
 
 describe('IDgenerator', () => {
     
-    const idGenerator = new StringRandomizer();
-
-    test('Testing the generation of a string of 8 hexadecimal characters', () => {
-        const id = idGenerator.generateId(8);
-        expect(id.length).toEqual(8);        
-    
-    })
 })
