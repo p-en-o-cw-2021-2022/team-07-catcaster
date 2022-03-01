fe3b348e0404c4c0cb6834b2a5a76dfe23875a58
Warre Heylen <warre.heylen@gmail.com>
2022-03-01 14:09:54 +0100

    Finished idDatabase class

--- src/server/classes.ts
+++ src/server/classes.ts
@@ -34,6 +34,38 @@ export class IdDatabase
     }
 
     public addScreen(id:string) {
+        if(this.doesIdExist(id)) {
+            return false;
+        }
+
+        this.ids.set(id,"screen");
+        return true;
+    }
+
+    public addController(id:string) {
+        if(this.doesIdExist(id)) {
+            return false;
+        }
+
+        this.ids.set(id,"controller");
+        return;
+    }
+
+    public removeScreen(id:string) {
+        if(!this.doesIdExist(id)) {
+            return false;
+        }
         
+        this.ids.delete(id);
+        return true;
+    }
+
+    public removeController(id:string) {
+        if(!this.doesIdExist(id)) {
+            return false;
+        }
+        
+        this.ids.delete(id);
+        return true;
     }
 }
