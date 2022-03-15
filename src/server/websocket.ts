ebab9bd405c92be9e3fb092905889f9b1eb9d197
Warre Heylen <warre.heylen@gmail.com>
2022-03-15 15:57:28 +0100

    added zod input checks

--- src/server/websocket.ts
+++ src/server/websocket.ts
@@ -1,5 +1,6 @@
 import ws, { WebSocketServer } from 'ws';
 import { database } from './index';
+import { z } from 'zod';
 
 export function websocketEventHandlers(websocket:ws.Server) {
 
@@ -8,31 +9,35 @@ export function websocketEventHandlers(websocket:ws.Server) {
         console.log('Connection established.');
 
         ws.on('message', (message) => {
+            const inputcheck = z.object({
+                client : z.string(),
+                id : z.string().refine((value:any) => database.doesIdExist(value), {
+                    message: 'Given ID does not exist.'
+                })
+            });
+
             const mes = JSON.parse(message.toString());
             console.log('Received message from: ', mes.id);
 
-            switch (mes.client) {
+            if (mes.succes) {
 
-            case 'screen':
-                console.log('Newly connected ID is from a screen.');
-                
-                //Het id dat door de client wordt doorgestuurd moet reeds bestaan.
-                if (!database.doesIdExist(mes.id)){
-                    console.log('Received ID is not in the database.');
-                    return;
-                }
-                //ws.send(JSON.stringify({type: 'ControllerID', id: mes.id}));
-                break
+                switch (mes.client) {
 
-            case 'controller':
-                console.log('Newly connected ID is from a controller.');
+                case 'screen':
+                    console.log('Newly connected ID is from a screen.');
+                    
+                    //Het id dat door de client wordt doorgestuurd moet reeds bestaan.
+                    if (!database.doesIdExist(mes.id)){
+                        console.log('Received ID is not in the database.');
+                        return;
+                    }
+                    //ws.send(JSON.stringify({type: 'ControllerID', id: mes.id}));
+                    break
 
-                //Het id dat door de client wordt doorgestuurd moet reeds bestaan.
-                if (!database.doesIdExist(mes.id)){
-                    console.log('Received ID is not in the database.');
-                    return;
-                }
+                case 'controller':
+                    console.log('Newly connected ID is from a controller.');
 
+<<<<<<< Updated upstream
                 //Send the controller the ID of the screen, as to establish a webRTC connection
                 let controllerid = database.getControllerIds();
                 websocket.clients.forEach(function(client){
@@ -41,9 +46,31 @@ export function websocketEventHandlers(websocket:ws.Server) {
                 let screenid = database.getScreenIds();
                 for(let sid of screenid!){
                     ws.send(JSON.stringify({client : 'screen', id : sid}))
+=======
+                    //Het id dat door de client wordt doorgestuurd moet reeds bestaan.
+                    if (!database.doesIdExist(mes.id)){
+                        console.log('Received ID is not in the database.');
+                        return;
+                    }
+
+                    //Send the controller the ID of the screen, as to establish a webRTC connection
+                    let controllerid = database.getControllerIds();
+                    websocket.clients.forEach(function(client){
+                        client.send(JSON.stringify({client: 'controller', id:controllerid}))
+                    });
+                    let screenid = database.getScreenIds();
+                    for(let sid of screenid!){
+                        ws.send(JSON.stringify({client : 'screen', id : sid}))
+                    }
+                    setTimeout((event) => {ws.send(JSON.stringify({client : 'connect', id : 0}))}, 500);
+                    break
+>>>>>>> Stashed changes
                 }
-                setTimeout((event) => {ws.send(JSON.stringify({client : 'connect', id : 0}))}, 500);
-                break
+            }
+
+            else {
+                console.log('Illegal input, closing connection...');
+                ws.close();
             }
         })
     });
