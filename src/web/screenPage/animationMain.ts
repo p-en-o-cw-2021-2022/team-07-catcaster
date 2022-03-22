5c256451728de439710fa3c78657adf38f070097
Jordy Carolus <jordy.carolus@student.kuleuven.be>
2022-03-22 16:26:10 +0100

    multiple controller with bug

--- src/web/screenPage/animationMain.ts
+++ src/web/screenPage/animationMain.ts
@@ -27,64 +27,83 @@ const planet: Planet = new Planet(scene, 0, 5, 10, [0,0,0]);
 const planet2: Planet = new Planet(scene, 1, 5, 10, [10,0,0]);
 planet.addNeighbour(planet2, new Vector3(3,0,0));
 planet2.addNeighbour(planet, new Vector3(-3,0,0));
-const cat: Cat = new Cat(scene, 0, 0.5, planet);
-planet.setCat(cat);
+//const cat: Cat = new Cat(scene, 0, 0.5, planet);
+//planet.setCat(cat);
+let cats = [];
 
 function animate() {
     const jumpdata = document.getElementById('jump')?.innerText;
-    if (jumpdata === 'true') {
-        cat.jump = true;
-    } else {
-        cat.jump = false;
-    }
-    // const gyrodata = document.getElementById('gyro-data')?.innerText;
-    // if ((gyrodata !== '') || (gyrodata !== undefined)) {
-    //     const datalist = gyrodata!.split(' ');
-    //     const beta = datalist[0];
-    //     const gamma = datalist[1];
-    //     console.log(beta);
-    //     console.log(gamma);
-    //     cat.xF = Number(gamma);
-    //     cat.yF = Number(beta);
+    // if (jumpdata === 'true') {
+    //     cat.jump = true;
+    // } else {
+    //     cat.jump = false;
     // }
-    cat.updatePosition(dt);
-    renderer.render( scene, camera );
-    setInnerText('xF', cat.xF);
-    setInnerText('yF', cat.yF);
-    setInnerText('xP', cat.position.x.toFixed(3));
-    setInnerText('yP', cat.position.y.toFixed(3));
-    setInnerText('zP', cat.position.z.toFixed(3));
-    setInnerText('angle', [planet.alpha, planet.beta, planet.gamma].toString());
-
-    requestAnimationFrame( animate );
-}
-
-function update(e: KeyboardEvent) {
-
-    switch(e.key) {
-    case 'd' :
-        // cat.updateForce('x', cat.xF + 5);
-        cat.xVel += 1;
-        break;
-    case 's' :
-        // cat.updateForce('y', cat.yF - 5);
-        cat.yVel -= 1;
-        break;
-    case 'w' :
-        // cat.updateForce('y', cat.yF + 5);
-        cat.yVel += 1;
-        break;
-    case 'a' :
-        // cat.updateForce('x', cat.xF - 5);
-        cat.xVel -= 1;
-        break;
+    let i = 5;
+    while (i>0) {
+        const gyrodata = document.getElementById('gyrodata_' + i)?.innerText;
+        if (typeof gyrodata !== 'undefined') {
+            if(cats.length<i){
+                eval('const cat' + i + ': Cat = new Cat(scene, 0, 0.5, planet);');
+                planet.setCat(eval('cat'+i));
+                cats.push(eval('cat'+i));
+            }
+            if (gyrodata !== '') {
+                const datalist = gyrodata!.split(' ');
+                const beta = datalist[0];
+                const gamma = datalist[1];
+                console.log(beta);
+                console.log(gamma);
+                eval('cat'+i).xF = Number(gamma);
+                eval('cat'+i).yF = Number(beta);
+            }
+            eval('cat'+i).updatePosition(dt);
+            renderer.render( scene, camera );
+            setInnerText('xF', eval('cat'+i).xF);
+            setInnerText('yF', eval('cat'+i).yF);
+            setInnerText('xP', eval('cat'+i).position.x.toFixed(3));
+            setInnerText('yP', eval('cat'+i).position.y.toFixed(3));
+            setInnerText('zP', eval('cat'+i).position.z.toFixed(3));
+            setInnerText('angle', [planet.alpha, planet.beta, planet.gamma].toString());
+        
+            requestAnimationFrame( animate );
+        }
+        i--;
     }
 }
+
+// function update(e: KeyboardEvent) {
+
+//     switch(e.key) {
+//     case 'd' :
+//         // cat.updateForce('x', cat.xF + 5);
+//         cat.xVel += 1;
+//         break;
+//     case 's' :
+//         // cat.updateForce('y', cat.yF - 5);
+//         cat.yVel -= 1;
+//         break;
+//     case 'w' :
+//         // cat.updateForce('y', cat.yF + 5);
+//         cat.yVel += 1;
+//         break;
+//     case 'a' :
+//         // cat.updateForce('x', cat.xF - 5);
+//         cat.xVel -= 1;
+//         break;
+//     }
+// }
 
 function update2(e: DeviceOrientationEvent) {
-    cat.updateForce('x', e.gamma!);
-    cat.updateForce('y', -e.beta!);
-}
+    let i = 5;
+    while (i>0) {
+        const gyrodata = document.getElementById('gyrodata_' + i)?.innerText;
+        if (typeof gyrodata !== 'undefined') {
+            eval('cat'+i).updateForce('x', e.gamma!);
+            eval('cat'+i).updateForce('y', -e.beta!);
+            }
+        }
+        i--;
+    }
 
 function firstTouch() {
     window.removeEventListener('touchend', firstTouch);
@@ -96,6 +115,6 @@ function firstTouch() {
     });
 }
 
-document.addEventListener('keypress', update);
+//document.addEventListener('keypress', update);
 window.addEventListener('touchend', firstTouch);
 animate();
