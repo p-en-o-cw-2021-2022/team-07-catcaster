25c86befdf3457512ae9811b484277996231465c
Olivier Vandenryt <olivier.vandenryt@student.kuleuven.be>
2022-03-29 14:53:10 +0200

    Code cleanup and linter fixes.

--- src/web/screenPage/screenPage.ts
+++ src/web/screenPage/screenPage.ts
@@ -1,45 +1,50 @@
 
-var myId = <HTMLDivElement>document.getElementById("receiver-id");
-myId.innerHTML = getIdScreen();
-var controllerId = null;
-eventHandlersScreen();
+const myId = <HTMLDivElement>document.getElementById('receiver-id');
+let controllerId = null;
 
-function getIdScreen() {
+interface Message {
+    'id': string;
+    'client': string;
+}
+
+interface WebSocketMessage {
+    'data': string;
+}
+
+function getIdScreen(): string | null {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const id: any = urlParams.get('id');
-    return id
+    const id: string | null = urlParams.get('id');
+    if (id) {
+        return id;
+    } else {
+        return null;
+    }
 }
-/*
-function getIdScreen(){
-    let currentUrl = window.location.href;
-    let result:string = "";
-    for(let i = 0; i < 8;i++){
-        let index = currentUrl.length - 8 + i;
-        result = result + currentUrl[index];
-    };
-    return result;
-}
-*/
 
 function eventHandlersScreen() {
-    let url = "wss" + window.location.href.substr(5);
+    const url = 'wss' + window.location.href.substr(5);
 
     const websocket = new WebSocket(url);
-    console.log("Starting Websocket connection...");
+    console.log('Starting Websocket connection...');
 
-    websocket.onopen = (event) => {
+    websocket.onopen = () => {
         console.log('Connection established.');
         websocket.send(JSON.stringify({client: 'screen', id: myId.innerHTML}));
     };
 
-    websocket.onmessage = (message:any) => {
-        let mes = JSON.parse(message.data);
+    websocket.onmessage = (message:WebSocketMessage) => {
+        const mes = <Message>JSON.parse(message.data);
         console.log('received message from : ', mes.id, '  |  client is: ', mes.client);
-        if(mes.client == 'controller'){
+        if(mes.client === 'controller') {
             controllerId = mes.id;
         }
     };
-};
+}
 
-export {myId, controllerId}
+if (getIdScreen() !== null) {
+    myId.innerHTML = <string>getIdScreen();
+}
+eventHandlersScreen();
+
+export {myId, controllerId};
