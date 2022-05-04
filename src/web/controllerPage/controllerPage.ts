787ce7b1078062e704692e2f27152baece475bc9
Jacob Schillemans <jacob.schillemans@student.kuleuven.be>
2022-05-04 16:20:05 +0200

    debug-info-controller

--- src/web/controllerPage/controllerPage.ts
+++ src/web/controllerPage/controllerPage.ts
@@ -1,6 +1,15 @@
 const id = <HTMLDivElement>document.getElementById('sender-id');
 const screenId = <HTMLDivElement>document.getElementById('receiver-id');
 const connectiondiv = <HTMLDivElement>document.getElementById('connection-div');
+const debug_controller = <HTMLButtonElement>document.getElementById("debug-info-controller");
+const controller_data =  <HTMLElement>document.getElementById("data");
+
+debug_controller.addEventListener('click',  function() {
+    
+    controller_data.hidden = !controller_data.hidden;
+    
+});
+
 
 interface Message {
     'id': string;
